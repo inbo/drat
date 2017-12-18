99765ebd7bee0f98d405e25ea3f473b9974e4ee9
ThierryO <Thierry.Onkelinx@inbo.be>
2017-12-18 17:31:49 +0100

    use ssh to clone repo

--- render.sh
+++ render.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git config --global user.email "bmk@inbo.be"
 git config --global user.name "INBO CI"
-git clone --depth=1 --branch=gh-pages https://$GITHUB_PAT:x-oauth-basic@github.com/inbo/drat.git
+git clone --depth=1 --branch=gh-pages git@github.com:inbo/drat.git
 cd drat
 if [[ $(git log -1 | grep "Render markdown") ]]; then
   echo "Done"
