c5ef6e7cc60f69db143e2bc62fb71e4568d575dd
ThierryO <Thierry.Onkelinx@inbo.be>
2017-12-19 00:46:57 +0100

    add github key before clone

--- render.sh
+++ render.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 git config --global user.email "bmk@inbo.be"
 git config --global user.name "INBO CI"
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 git clone --depth=1 --branch=gh-pages git@github.com:inbo/drat.git
 cd drat
 if [[ $(git log -1 | grep "Render markdown") ]]; then
   echo "Done"
 else
-  ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
   git checkout gh-pages
   Rscript "render.R"
   git add --all
