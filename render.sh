07310c8a4193b60f3bdb11c37824133136fa1220
ThierryO <Thierry.Onkelinx@inbo.be>
2017-12-18 11:42:04 +0100

    clone correct repo

--- render.sh
+++ render.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git config --global user.email "bmk@inbo.be"
 git config --global user.name "INBO CI"
-git clone --depth=1 --branch=gh-pages https://$GITHUB_PAT@github.com/ThierryO/drat.git
+git clone --depth=1 --branch=gh-pages https://$GITHUB_PAT@github.com/inbo/drat.git
 cd drat
 if [[ $(git log -1 | grep "Render markdown") ]]; then
   echo "Done"
